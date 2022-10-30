356fe31205de6ca499178637b524d39ce5c6d69a
Salmi Konsta S <kxsalmi@cubbli20-gold.local>
2022-10-31 00:23:28 +0200

    Progress

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,25 +84,22 @@ DATABASES = {
     }
 }
 
+AUTH_PASSWORD_VALIDATORS = [] # Security flaw!: Password validation left out for ease of development. Fix below:
 
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
+# AUTH_PASSWORD_VALIDATORS = [
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#    },
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#    },
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#    },
+#    {
+#        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#    },
+#]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
